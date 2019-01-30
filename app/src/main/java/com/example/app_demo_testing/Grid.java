a31825d410543f97fa793cea430d5952f0d86614
unknown <nikeshp96@gmail.com>
2019-01-29 16:17:18 -0800

    First Commit

---
+++ app/src/main/java/com/example/app_demo_testing/Grid.java
@@ -0,0 +1,56 @@
+package com.example.app_demo_testing;
+
+import android.graphics.Point;
+
+import java.util.Random;
+
+public class Grid {
+
+    private boolean[][] grid;
+    public int moves;
+
+    public Grid(){
+        grid = new boolean[3][3];
+        moves =0;
+    }
+
+    public boolean isOn(Point x){
+        return grid[x.y][x.x];
+    }
+
+    public void setUp(){
+        Random randy = new Random();
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                grid[i][j] = randy.nextBoolean();
+            }
+        }
+        moves = 0;
+    }
+
+
+    private final Point[] steps = {new Point(0,1) , new Point(1,0), new Point(-1,0) , new Point(0,-1),new Point(0,0)};
+
+    public void Toggle(Point x) {
+
+        for (Point pivot : steps) {
+            Point temp = new Point(x.x + pivot.x, x.y + pivot.y);
+            if (temp.x >= 3 || temp.x < 0 || temp.y >= 3 || temp.y < 0)
+                continue;
+            grid[temp.y][temp.x] = !grid[temp.y][temp.x];
+        }
+        moves++;
+    }
+
+    public boolean isGameOver(){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(!grid[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
---
+++ app/src/main/java/com/example/app_demo_testing/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.app_demo_testing;
+
+import android.graphics.Point;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Grid gameGrid;
+    GridLayout grid;
+    TextView score;
+
+    Button[][] grid_buttons;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gameGrid = new  Grid();
+        grid =  findViewById(R.id.light_grid);
+        score = findViewById(R.id.Score);
+        grid_buttons = new Button[3][3];
+        int index = 0;
+
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                grid_buttons[i][j] = (Button)grid.getChildAt(index);
+                index++;
+            }
+        }
+        gameGrid.setUp();
+        PaintGrid();
+    }
+
+    public void PaintGrid(){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(gameGrid.isOn(new Point(i,j)))
+                    grid_buttons[j][i].setBackgroundColor(ContextCompat.getColor(this,R.color.colorOn));
+                else
+                    grid_buttons[j][i].setBackgroundColor(ContextCompat.getColor(this,R.color.colorOff));
+
+            }
+        }
+    }
+
+
+    public void Toggle(View view) {
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(view==grid_buttons[j][i])
+                    gameGrid.Toggle(new Point(i,j));
+            }
+        }
+
+        PaintGrid();
+        if(gameGrid.isGameOver())
+            Toast.makeText(this, "You Win!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void restartGame(View view) {
+        gameGrid.setUp();
+        PaintGrid();
+    }
+}
